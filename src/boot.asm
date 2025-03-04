09c710aa24d29a7d9bc0087a7d44e48cf05c5eb8
STierProgrammer <150156387+STierProgrammer@users.noreply.github.com>
2025-03-05 00:16:03 +0400

    Printing out the alphabet

--- src/boot.asm
+++ src/boot.asm
@@ -1,3 +1,17 @@
+mov ah, 0x0E ; BIOS teletype mode
+mov al, 65 ; ASCII code for 'A'
+int 0x10
+
+loop: 
+    inc al
+    cmp al, 91
+    je exit
+    int 0x10
+    jmp loop 
+
+exit:
+    jmp $
+
 jmp $
 
 times 510-($-$$) db 0
